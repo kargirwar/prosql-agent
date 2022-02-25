abe973ceb8cd9c9ff377ea4e19e1003f268cd656
Pankaj Kargirwar <kargirwar@gmail.com>
2022-02-25 16:57:53 +0530

    Added private network access header

--- main.go
+++ main.go
@@ -113,7 +113,7 @@ func mw(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Private-Network", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "X-Request-ID")
 		w.Header().Set("Content-Type", "application/json")
 
